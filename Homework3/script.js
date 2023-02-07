6d88c9cdbd14f9f05c6942b3709f83b48885f764
Eduard <eduard_mayburov@mail.ru>
2023-02-07 15:35:33 +0700

    Homework3 done

---
+++ Homework3/script.js
@@ -0,0 +1,86 @@
+/* Задание 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+function NumInKub(number) {
+    number = number * number * number;
+    return number
+}
+
+console.log(NumInKub(2) + NumInKub(3))
+
+/* Задание 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+function DeductionOfTaxes(num) {
+    num = num * 0.87;
+  
+if (num >= 1){
+    return num
+}
+else {
+    num = 'Неверный ввод данных'
+    return num
+}
+}
+
+let num = prompt();
+console.log(DeductionOfTaxes(num))
+
+/* Задание 3 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+function MaxDigit(num1, num2, num3){
+    let max = num1
+    if (max < num2) {
+        max = num2
+    }
+    if (max < num3){
+        max = num3
+    }
+    return max
+}
+
+let num1 = prompt()
+let num2 = prompt()
+let num3 = prompt()
+
+console.log(MaxDigit(num1,num2,num3))
+
+/* Задание 4 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+function sum(num1,num2){
+    let sum = 0
+    sum = num1 + num2
+    return sum
+}
+
+function dif(num1,num2) {
+    let dif = 0
+    if (num1 < num2){
+        dif = num2 - num1
+        return dif
+    }
+    else {
+        dif = num1 - num2
+        return dif
+    }
+}
+
+function mult(num1,num2){
+    let mult = 0
+    mult = num1 * num2
+    return mult
+}
+
+function div(num1,num2){
+    let div = 0
+    div = num1 / num2
+    return div
+}
+
+let num1 = prompt()
+num1 = Number(num1)
+let num2 = prompt()
+num2 = Number(num2)
+
+console.log(sum(num1,num2))
+console.log(dif(num1,num2))
+console.log(mult(num1,num2))
+console.log(div(num1,num2))
